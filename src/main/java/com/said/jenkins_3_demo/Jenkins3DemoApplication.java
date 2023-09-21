4e3ba1ba6f6ad5e0e9245f1e108167ce3bbb1197
said <said1024.b@gmail.com>
2023-09-21 14:56:44 +0100

    first c

---
+++ src/main/java/com/said/jenkins_3_demo/Jenkins3DemoApplication.java
@@ -0,0 +1,14 @@
+package com.said.jenkins_3_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkins3DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jenkins3DemoApplication.class, args);
+		System.out.println("jenkins3");
+	}
+
+}
