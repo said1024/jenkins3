de98b33495985242244a5b9195142191b86adaa7
said1024 <111237772+said1024@users.noreply.github.com>
2023-09-27 15:58:36 +0100

    commit 1
    commit 1

--- src/main/java/com/said/jenkins_3_demo/Jenkins3DemoApplication.java
+++ src/main/java/com/said/jenkins_3_demo/Jenkins3DemoApplication.java
@@ -8,7 +8,7 @@ public class Jenkins3DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Jenkins3DemoApplication.class, args);
-		System.out.println("jenkins3 modified");
+		System.out.println("jenkins3 modified 1");
 	}
 
 }
