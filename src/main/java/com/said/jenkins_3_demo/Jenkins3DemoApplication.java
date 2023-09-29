7fe365e3f4565a77675d27210fd3fa62947c716b
said1024 <111237772+said1024@users.noreply.github.com>
2023-09-29 14:56:52 +0100

    Update_10

--- src/main/java/com/said/jenkins_3_demo/Jenkins3DemoApplication.java
+++ src/main/java/com/said/jenkins_3_demo/Jenkins3DemoApplication.java
@@ -8,7 +8,7 @@ public class Jenkins3DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Jenkins3DemoApplication.class, args);
-		System.out.println("jenkins9");
+		System.out.println("jenkins_10");
 
 	}
 
