0ebc94377bd7276704ec4da03683e4ceb888e032
said <said1024.b@gmail.com>
2023-09-29 15:03:13 +0100

    web hook 11

--- src/main/java/com/said/jenkins_3_demo/Jenkins3DemoApplication.java
+++ src/main/java/com/said/jenkins_3_demo/Jenkins3DemoApplication.java
@@ -8,7 +8,7 @@ public class Jenkins3DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Jenkins3DemoApplication.class, args);
-		System.out.println("jenkins8");
+		System.out.println("jenkins 11");
 
 	}
 
