488ef3977acbfe3182e44ec9b2526157d84e763f
said <said1024.b@gmail.com>
2023-09-29 16:56:24 +0100

    test 15

--- src/main/java/com/said/jenkins_3_demo/Jenkins3DemoApplication.java
+++ src/main/java/com/said/jenkins_3_demo/Jenkins3DemoApplication.java
@@ -8,7 +8,7 @@ public class Jenkins3DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Jenkins3DemoApplication.class, args);
-		System.out.println("jenkins_13");
+		System.out.println("jenkins_15");
 	}
 
 }
